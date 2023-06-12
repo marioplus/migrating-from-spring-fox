8a42cdd5bdd285f1236797b502a168835087d6cd
marioplus <nokiadafa@live.com>
2023-06-12 23:19:09 +0800

    update: 调整不准确的注解用法

--- spring-fox-demo/src/main/java/net/marioplus/springfoxdemo/UserRest.java
+++ spring-fox-demo/src/main/java/net/marioplus/springfoxdemo/UserRest.java
@@ -9,14 +9,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @RequestMapping("/user")
 public class UserRest {
 
-    @ApiOperation(value = "get", notes = "查找用户")
+    @ApiOperation(value = "查找用户", notes = "查找用户")
     @ApiImplicitParam(name = "id", value = "编号", required = true, dataTypeClass = Long.class)
     @GetMapping
     public User get(@RequestParam Long id) {
         return new User(id, "", 12);
     }
 
-    @ApiOperation(value = "add", notes = "新增用户")
+    @ApiOperation(value = "新增用户", notes = "新增用户")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "id", value = "编号", required = true, dataTypeClass = Long.class),
             @ApiImplicitParam(name = "name", value = "姓名", required = true, dataTypeClass = String.class),
@@ -27,13 +27,11 @@ public class UserRest {
         return new User().getId();
     }
 
-    @ApiOperation(value = "update", notes = "更新用户")
+    @ApiOperation(value = "更新用户", notes = "更新用户")
     @ApiImplicitParams(
             @ApiImplicitParam(name = "id", value = "编号", required = true, dataTypeClass = Long.class)
     )
-
     @ApiResponse(code = 404, message = "foo")
-    @ApiIgnore(value = "")
     @PutMapping
     public Long update(@RequestParam Long id, @RequestBody User user) {
         return user.getId();
